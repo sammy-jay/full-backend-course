815c3f7d90362866a421387337fcf144b6158610
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-26 11:29:24 +0000

    learnt about using pydantic models (#3)

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
+
+class Post(BaseModel):
+    title: str
+    content: str
+
+
 @app.get("/")
 def index():
-    return "Hello World"
+    return {"msg": "Welcome to my API"}
+
+
+@app.get("/posts/")
+def get_posts():
+    return []
+
+
+@app.post("/posts/")
+def create_post(post: Post):
+    print(post.title)
+    return {}
